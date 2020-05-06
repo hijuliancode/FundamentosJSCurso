98be52f11126e0345de7e3d0b9ff2ad5e105613a
JulianCode <hijuliansosa@protonmail.com>
2020-05-06 00:12:19 -0500

    Clase 5 - Alcance de las funciones

--- clase5.js
+++ clase5.js
@@ -1,8 +1,14 @@
-var nombre = 'Sacha'
+var nombre = 'Sacha' // Variable de alcance global (window en este caso)
+window.nombre; // Accedemos a la variable que esta en el contexto global
 
-function imprimirNombreEnMayusculas(nombre) {
+function imprimirNombreEnMayusculas() {
   nombre = nombre.toUpperCase()
   console.log(nombre)
 }
+imprimirNombreEnMayusculas() // Ejecutamos y modificamos el contexto clobal
 
-imprimirNombreEnMayusculas(nombre)
+function imprimirNombreEnMayusculas(nombre) {
+  nombre = nombre.toUpperCase() // a pesar de tener el mismo nombre de la variable gloal, no hace referencía pues esta es una variable local
+  console.log(nombre)
+}
+imprimirNombreEnMayusculas(nombre) // al crear un parametro creamos una variable local y no ensuciamos el contexto global
