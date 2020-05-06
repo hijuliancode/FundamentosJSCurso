b101f72cd87a58f2bbd48ccf822d3f6773e214e4
JulianCode <hijuliansosa@protonmail.com>
2020-05-05 23:24:10 -0500

    Clase 1: Variables

--- clase1.js
+++ clase1.js
@@ -1,7 +1,19 @@
-var nombre = 'Sacha', apellido = 'Lifszyc'
-var edad = 28
+// Declaración de variables
+var nombre,
+    apellido; 
 
+// Inicialización, definicion de variables
+nombre = 'Julian'
+apellido = 'Sosa'
+
+
+
+var edad = 28 // Se puede declarar e inicializar en la misma linea
 edad = '28 años'
+
+// Javascript es un lenguaje debilmente tipado,
+// Significa que no hay nada que diga o obligue a una variable
+// a ser de determinado tipo (string, boolean, number ...)
 
 console.log('Hola ' + nombre + ' ' + apellido)
 console.log('Tengo ' + edad)
