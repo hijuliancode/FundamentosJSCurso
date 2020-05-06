e8b76bef82114c37bd052332e7934e53d8de3ceb
JulianCode <hijuliansosa@protonmail.com>
2020-05-06 09:17:03 -0500

    Clase 6 - Objetos

--- clase6.js
+++ clase6.js
@@ -1,5 +1,5 @@
 var sacha = {
-  nombre: 'Sacha',
+  nombre: 'Sacha', // Clave, valor
   apellido: 'Lifszyc',
   edad: 28
 }
@@ -10,11 +10,25 @@ var dario = {
   edad: 27
 }
 
-function imprimirNombreEnMayusculas({ nombre }) {
+// function imprimirNombreEnMayusculas(nombre) { // Opción 1
+//   console.log(nombre.toUpperCase())
+// }
+
+// function imprimirNombreEnMayusculas(persona) { // Opción 2
+//   var nombre = persona.nombre;
+//   console.log(nombre)
+// }
+
+// function imprimirNombreEnMayusculas({ nombre }) { // Opción 3
+//   console.log(nombre.toUpperCase())
+// }
+
+function imprimirNombreEnMayusculas({ nombre }) { // Opción 3, nuevas versiones de JS, destructurar el objeto en el argumento
   console.log(nombre.toUpperCase())
 }
 
 imprimirNombreEnMayusculas(sacha)
 imprimirNombreEnMayusculas(dario)
-// imprimirNombreEnMayusculas({ nombre: 'Pepito' })
-imprimirNombreEnMayusculas({ apellido: 'Gomez' })
+// imprimirNombreEnMayusculas({ nombre: 'Pepito' }) 
+imprimirNombreEnMayusculas() // Error, no se esta pasando un atributo y la función espera uno
+imprimirNombreEnMayusculas({ apellido: 'Gomez' }) // Error, el objeto no tiene el atributo nombre
